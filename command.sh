fd0fb4b75e7b9bc3bdc58dba651ec2ea5ca711cf
Houjun <hliu@shabang.cf>
2020-09-10 12:19:28 -0700

    Bonk

--- command.sh
+++ command.sh
@@ -9,20 +9,16 @@ while true; do
 
     changes="$(git status --porcelain | cut -c4-)"
 
-    for file in $to_compile; do
-        echo $file
-    done
-
-    exit
-
     if [[ -n "$changes" ]]; then
         echo "Log for attempt at $(date)" > recent_errors.txt
         find . -type f -name '*.md' -exec sed -i'' -e 's/\/Users\/houliu\/Documents\/School Work\/2020\-2021\/KnowledgeBase/./g' {} +
 
-    for f in ${(@f)changes}; do
-        pandoc -f markdown -t pdf   $< --pdf-engine=xelatex --mathjax -o $@ --template=./meta/templates/default.latex --resource-path=$(call SUBJECT,$@)
-        pandoc -f markdown -t latex $< --pdf-engine=xelatex --mathjax -o $@ --template=./meta/templates/default.latex --resource-path=$(call SUBJECT,$@)
-        pandoc -f latex    -t html  $< --pdf-engine=xelatex --mathjax -o $@ --template=./meta/templates/default.html --resource-path=$(call SUBJECT,$@)
+        for f in ${(@f)$(echo $changes |  grep -E '(\.md|\.org)$')}; do
+            echo "Converting $f to ${f%.*}.{pdf, latex, html} failed"
+            #pandoc -f markdown -t pdf   $f --pdf-engine=xelatex --mathjax -o "${f%.*}.pdf"   --template=./meta/templates/default.latex --resource-path=$(call SUBJECT,"${f%.*}")
+            #pandoc -f markdown -t latex $f --pdf-engine=xelatex --mathjax -o "${f%.*}.latex" --template=./meta/templates/default.latex --resource-path=$(call SUBJECT,"${f%.*}")
+            #pandoc -f latex    -t html  $f --pdf-engine=xelatex --mathjax -o "${f%.*}.html"  --template=./meta/templates/default.html --resource-path=$(call SUBJECT,"${f%.*}")
+        done
 
         #if make >> log.txt 2>>recent_errors.txt; then
         #    [[ -f $BUILDNUMBER_FILE ]] || echo 0 > $BUILDNUMBER_FILE;
