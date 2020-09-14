131def3bb68eb385b0c41522d34701f989bc91ff
Exr0nStatic <exr0n.static@gmail.com>
2020-09-13 22:49:15 -0700

    b174: compile.sh (automated compilation 174)

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ changes="$(git status --porcelain | cut -c4- | grep -E '(\.md|\.org)$')"
 for f in ${(@f)changes}; do
     formatname="$filetype[${f:t:e}]"
     echo "Log for attempt at $(date)" >> recent_errors.txt
-    echo "Converting $f from $formatname to ${f%.*}.{pdf, latex, html}"
+    echo "$(date) Converting $f from $formatname to ${f%.*}.{pdf, latex, html}"
     pandoc -f $formatname -t pdf   $f --pdf-engine=xelatex --mathjax -o "${f%.*}.pdf" \
         --template=~/.pandoc/templates/default.latex --resource-path="$f:h" 2>>recent_errors.txt
     echo "    compiled to pdf"
