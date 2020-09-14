56a51ec0fdbce7b83cc91a3694d5b824ea2a7165
Exr0nStatic <exr0n.static@gmail.com>
2020-09-13 22:48:31 -0700

    b173: compile.sh (automated compilation 173)

--- compile.sh
+++ compile.sh
@@ -7,16 +7,16 @@ filetype[org]=org
 changes="$(git status --porcelain | cut -c4- | grep -E '(\.md|\.org)$')"
 for f in ${(@f)changes}; do
     formatname="$filetype[${f:t:e}]"
-    echo "Log for attempt at $(date)" > recent_errors.txt
+    echo "Log for attempt at $(date)" >> recent_errors.txt
     echo "Converting $f from $formatname to ${f%.*}.{pdf, latex, html}"
     pandoc -f $formatname -t pdf   $f --pdf-engine=xelatex --mathjax -o "${f%.*}.pdf" \
-        --template=~/.pandoc/templates/default.latex --resource-path="$f:h" 2>recent_errors.txt
+        --template=~/.pandoc/templates/default.latex --resource-path="$f:h" 2>>recent_errors.txt
     echo "    compiled to pdf"
     pandoc -f $formatname -t latex $f --pdf-engine=xelatex --mathjax -o "${f%.*}.latex" \
-        --template=~/.pandoc/templates/default.latex --resource-path="$f:h" 2>recent_errors.txt
+        --template=~/.pandoc/templates/default.latex --resource-path="$f:h" 2>>recent_errors.txt
     echo "    compiled to latex"
     pandoc -f latex -t html "${f%.*}.latex" --pdf-engine=xelatex --mathjax -o "${f%.*}.html"\
-        --template=~/.pandoc/templates/default.html  --resource-path="$f:h" 2>recent_errors.txt
+        --template=~/.pandoc/templates/default.html  --resource-path="$f:h" 2>>recent_errors.txt
     echo "    compiled to html"
 done
 
