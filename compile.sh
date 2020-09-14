a9e078bc7f7507bdc0baa1480d0131c46bd8f104
Exr0nStatic <exr0n.static@gmail.com>
2020-09-13 19:59:45 -0700

    b132: compile.sh,test.latex test.org,#test.latex# (automated compilation 132)

--- compile.sh
+++ compile.sh
@@ -12,7 +12,5 @@ for f in ${(@f)1}; do
     pandoc -f $formatname -t html  $f --pdf-engine=xelatex --mathjax -o "${f%.*}.html" --template=~/.pandoc/templates/default.html
 done
 
+make flush
 
-#pandoc -f markdown -t pdf   $f --pdf-engine=xelatex --mathjax -o "${f%.*}.pdf"   --template=./meta/templates/default.latex --resource-path=$(call SUBJECT,"${f%.*}")
-#pandoc -f markdown -t latex $f --pdf-engine=xelatex --mathjax -o "${f%.*}.latex" --template=./meta/templates/default.latex --resource-path=$(call SUBJECT,"${f%.*}")
-#pandoc -f latex    -t html  $f --pdf-engine=xelatex --mathjax -o "${f%.*}.html"  --template=./meta/templates/default.html --resource-path=$(call SUBJECT,"${f%.*}")
