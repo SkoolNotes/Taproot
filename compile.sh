187aa8a8b6bcfb7792cf0662c138ab31a85f2156
Exr0nStatic <exr0n.static@gmail.com>
2020-09-15 14:39:59 -0700

    better compile.sh output
    co-authored-by: Exr0n <mail@exr0n.com>

--- compile.sh
+++ compile.sh
@@ -8,16 +8,16 @@ changes="$(git status --porcelain | cut -c4- | grep -E '(\.md|\.org)$')"
 for f in ${(@f)changes}; do
     formatname="$filetype[${f:t:e}]"
     echo "Log for attempt at $(date)" >> recent_errors.txt
-    echo "$(date) Converting $f from $formatname to ${f%.*}.{pdf, latex, html}\r"
+    printf "$(date) Converting $f from $formatname to ${f%.*}.{pdf, latex, html}\r"
     pandoc -f $formatname -t pdf   $f --pdf-engine=xelatex --mathjax -o "${f%.*}.pdf" \
         --template=~/.pandoc/templates/default.latex --resource-path="$f:h" 2>>recent_errors.txt
-    echo "1/3:     "
+    printf "1/3:     \r"
     pandoc -f $formatname -t latex $f --pdf-engine=xelatex --mathjax -o "${f%.*}.latex" \
         --template=~/.pandoc/templates/default.latex --resource-path="$f:h" 2>>recent_errors.txt
-    echo "2/3:     "
+    printf "2/3:     \r"
     pandoc -f latex -t html "${f%.*}.latex" --pdf-engine=xelatex --mathjax -o "${f%.*}.html"\
         --template=~/.pandoc/templates/default.html  --resource-path="$f:h" 2>>recent_errors.txt
-    echo "3/3:     "
+    echo "$(date) Converted $f from $formatname to ${f%.*}.{pdf, latex, html}"
 done
 
 make flush >> meta/log.txt
