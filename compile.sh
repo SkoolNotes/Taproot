8a85f9068e929c82e750d85af016a1591725ab42
Exr0nStatic <exr0n.static@gmail.com>
2021-04-28 20:44:45 -0700

    b12255: compile.sh (automated compilation 12255)

--- compile.sh
+++ compile.sh
@@ -29,7 +29,7 @@ for f in ${(@f)changes}; do
     fi
     if [[ $formatname == "org" ]]; then
         # t2proot only works with markdown
-
+        pandoc $f --pdf-engine=xelatex --mathjax -s
     fi
     pandoc -f $formatname -t pdf   $f --pdf-engine=xelatex --mathjax -s\
         --template=~/.pandoc/templates/default.latex -o "${f%.*}.pdf"\
--- recent_errors.txt
+++ recent_errors.txt
@@ -1,1 +1,1 @@
-        Log for attempt at Wed Apr 28 08:43:38 PM PDT 2021 (12254)
+        Log for attempt at Wed Apr 28 08:44:44 PM PDT 2021 (12255)
